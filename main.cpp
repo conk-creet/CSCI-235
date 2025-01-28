6db2bda772bc773476565b8bb3712caeef4aa29d
ConCrete <89358773+conk-creet@users.noreply.github.com>
2025-01-28 11:01:43 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//File: main.cpp
+//Author: Evan Feusi
+//Date: 01/28/25
+/* A Simple C++ program which prints out "Hello, World!" */
+
+int main(){
+  str::cout << "hello, world!" << std::endl;
+  return 0;
+}
